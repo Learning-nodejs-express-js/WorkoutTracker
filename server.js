420cbc236f2401306050e8b2536b6d9033b7e0e0
VISH01AL <g.vish1al@gmail.com>
2024-02-05 11:40:56 -0500

    starting mongodb

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 require("dotenv").config()
-
 const express=require("express")
-const router=require("./routes/workouts.js")
 const app=express()
-app.listen(process.env.PORT,()=>{
-    console.log("server is running",process.env.PORT)
-})
+const workoutRouter=require("./Routes/workout")
+
+app.use(express.json())
 
 app.use((req,res,next)=>{
-    console.log(req.path,req.method)
-    next()
+console.log(req.path,req.method)
+next()
 })
 
-app.use("/workouts",router)
+app.use("/workouts",workoutRouter)
+
+
+app.listen(process.env.PORT,()=>{
+    console.log(process.env.PORT,"listening on 4000")
+})
+
+//testing
+// app.get("/",(req,res)=>{
+//     res.json({msg:"hello world from get"})
+// })
